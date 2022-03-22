040603efd9e0608e18e65f1310f79440d913173e
qianzhuoyu <101181308+qianzhuoyu@users.noreply.github.com>
2022-03-22 12:04:11 +0800

    Add files via upload

---
+++ practical5/pizza_party.py
@@ -0,0 +1,11 @@
+# we need to define two variables.
+n = 0
+p = 0
+# whether p is greater or equal to 64
+#    if yes:done
+#    if no: keep looping
+while p < 64:
+    n += 1
+    p = (n ** 2 + n + 2) / 2
+# print a sentence
+print(n, "straight cuts can have", p, "pizza slices for each member of the IBI1 class")
---
+++ practical5/triangle.py
@@ -0,0 +1,11 @@
+# we need to define a variable first.
+j = 1
+# there is an equation y=x*(x+1)/2 between j and k.  k is the dot of the triangle.
+# k==j*(j+1)/2
+# we need a loop to increase the j to calculate the k.
+while j < 11:
+    k = j * (j + 1) / 2
+# print a sentence
+    print('triangle', j, 'has', k, 'dots')
+    # the j should be added one by one.
+    j = j + 1
