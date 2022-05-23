50b9d6dbd27dd09d45d5048370d1aae53dbde280
qianzhuoyu <101181308+qianzhuoyu@users.noreply.github.com>
2022-05-24 01:21:52 +0800

    Add files via upload

---
+++ practical11/Alignment.py
@@ -0,0 +1,90 @@
+# import three .fa files
+human = open('DLX5_human.fa')
+mouse = open('DLX5_mouse.fa')
+random = open('RandomSeq(1).fa')
+random_sequence = random.readlines()  # read all lines in the files and store the lines in random_sequence
+human_sequence = human.readlines()
+mouse_sequence = mouse.readlines()
+random_seq = random_sequence[1].strip()  # Remove the first line, and all that's left is the amino acid sequence,
+# store in random_seq
+human_seq = human_sequence[1].strip()
+mouse_seq = mouse_sequence[1].strip()
+
+
+def percentage_identical(seq1, seq2):  # define a function to compare two amino sequences one by one
+    distance = 0
+    for i in range(len(mouse_seq)):
+        if seq1[i] == seq2[i]:
+            distance += 1
+    percentage = distance / len(mouse_seq) * 100  # Calculate the percentage of similarity
+    return percentage
+
+
+# Run the function to get the percentage of similarity between the three sequences
+print('The percentage of identical amino acids between random and human is:',
+      percentage_identical(random_seq, human_seq), end='')
+print('%')
+print('The percentage of identical amino acids between random and mouse is:',
+      percentage_identical(random_seq, mouse_seq), end='')
+print('%')
+print('The percentage of identical amino acids between mouse and human is:',
+      percentage_identical(mouse_seq, human_seq), end='')
+print('%')
+
+
+# create a list to store abbreviation of amino acids
+amino_acid_list = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'x']
+# import the BLOSUM62. The scores in the BLOSUM62 correspond to the sequence of amino acids in the amino_acid_list
+BLOSUM62 = [
+            [ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0],
+            [-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1],
+            [-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1],
+            [-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1],
+            [ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2],
+            [-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1],
+            [-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1],
+            [ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1],
+            [-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1],
+            [-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1],
+            [-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1],
+            [-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1],
+            [-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1],
+            [-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1],
+            [-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2],
+            [ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0],
+            [ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0],
+            [-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2],
+            [-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1],
+            [ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1],
+            [-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1],
+            [-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1],
+            [ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1],
+
+]
+
+
+def BLOSUM_score(seq1, seq2):  # define a function to calculate the total score
+    score = 0
+    for i in range(len(seq1)):  # match the amino acid in seq1 with the abbreviation in the amino_acid_list
+        for l in range(len(amino_acid_list)):
+            if seq1[i] == amino_acid_list[l]:
+                x = l  # position of seq1's amino in the amino_acid_list = x
+                break
+        for l in range(len(amino_acid_list)):
+            if seq2[i] == amino_acid_list[l]:
+                y = l  # position of seq2's amino in the amino_acid list = y
+                break
+        score = score + BLOSUM62[x][y]  # find the score and add up all scores
+    return score
+
+
+# run the function to calculate the score
+score1 = BLOSUM_score(mouse_seq, human_seq)
+score2 = BLOSUM_score(mouse_seq, random_seq)
+score3 = BLOSUM_score(human_seq, random_seq)
+
+
+print('\n')
+print('The score between mouse and human is:', score1)
+print('The score between mouse and random is:', score2)
+print('The score between random and human is:', score3)
